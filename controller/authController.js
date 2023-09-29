65458af11a61aed198c5894162deb24fb8e5d80a
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-09-29 18:35:41 +0200

    update

--- controller/authController.js
+++ controller/authController.js
@@ -37,6 +37,7 @@ exports.signIn = (req, res) => {
             const token = jwt.sign(
               {
                 data: user.email,
+                user: user.firstname,
                 role: user.id_role,
                 id: user.id,
               },
